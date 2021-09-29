a3dec3a500da6728a24730ad22a69ebece4f409c
Julián Obando <julianobando@unicomfacauca.edu.co>
2021-09-29 17:37:10 -0500

    Limpiar view people

--- conectivity/ApiRest/views.py
+++ conectivity/ApiRest/views.py
@@ -55,21 +55,3 @@ def people_detail(request,key):
 
 # conversion()
 
-#Número primo
-@api_view(['GET'])
-def getPrimeNumer(lista, n):
-    lista = int(input('¿Hasta qué número quieres la lista?: '))
-    cont = 0
-    for i in range(2, lista + 1):
-        primos = True
-        for j in range(2,11):
-            if i == j:
-                break
-            elif i%j == 0:
-                primos = False
-            else:
-                continue
-        if primos == True:
-            print(' ',i, end='')
-            cont += 1
-    print('\nHay %u números primos.' % cont )
